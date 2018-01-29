788519a6f7b3915c8a8ab83e105f055b6bf38721
Tao Wu <wtomybo@gmail.com>
2018-01-29 22:04:27 +0800

    CPP: Inherit public/virtual

---
+++ cpp/inherit/public.cpp
@@ -0,0 +1,62 @@
+#include <iostream>
+ 
+using namespace std;
+ 
+class Shape 
+{
+   public:
+      Shape(){
+	cout << "Shape Construct" << endl;
+      }
+
+      void setWidth(int w)
+      {
+         width = w;
+      }
+      void setHeight(int h)
+      {
+         height = h;
+      }
+   protected:
+      int width;
+      int height;
+};
+ 
+class PaintCost 
+{
+   public:
+      PaintCost() {
+        cout << "PaintCost Construct" << endl;
+      }
+      int getCost(int area)
+      {
+         return area * 70;
+      }
+};
+ 
+class Rectangle: public Shape, public PaintCost
+{
+   public:
+      int getArea()
+      { 
+         return (width * height); 
+      }
+};
+ 
+int main(void)
+{
+   Rectangle Rect;
+   int area;
+ 
+   Rect.setWidth(5);
+   Rect.setHeight(7);
+ 
+   area = Rect.getArea();
+   
+   cout << "Total area: " << Rect.getArea() << endl;
+ 
+   cout << "Total paint cost: $" << Rect.getCost(area) << endl;
+ 
+   return 0;
+}
+
