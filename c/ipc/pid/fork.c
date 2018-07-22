df8aa70930192301f6cf63c26f04233db0d9f621
Tao Wu <wtomybo@gmail.com>
2018-07-22 09:23:13 +0800

    c/ipc/pid: Update waitpid() in fork()

--- c/ipc/pid/fork.c
+++ c/ipc/pid/fork.c
@@ -1,24 +1,31 @@
 #include <stdio.h>
 #include <unistd.h>
 #include <stdlib.h>
-main()
+#include <sys/wait.h>
+
+int main(int argc, char *argv[])
 {
-    fprintf(stderr,"fork...\n");
-    if(fork() == 0 )
-    {
-        printf("1. process id=%d\n",getpid());
-        printf("parent process id=%d\n",getppid());
-        printf("process group id=%d\n",getpgrp());
-        printf("wait...");
-        wait();
-        printf("wait end\n");
-        exit(0);
-    }
-        printf("2. process id=%d\n",getpid());
-        printf("parent process id=%d\n",getppid());
-        printf("process group id=%d\n",getpgrp());
-        printf("sleep...");
-        sleep(10);
-        printf("sleep end\n");
-        exit(0);
+	int pid = 0;
+	int ret = 0;
+	int status = 0;
+
+	printf("fork start\n");
+	pid = fork();
+	if (pid == 0) {
+		printf("Child: pgrp=%d, ppid=%d, pid=%d\n", getpgrp(), getppid(), getpid());
+		printf("sleep...\n");
+		sleep(3);
+		printf("sleep end\n");
+		exit(0);
+	} else {
+		printf("Parent: pgrp=%d, ppid=%d, pid=%d\n", getpgrp(), getppid(), getpid());
+		printf("wait...\n");
+		#if 0
+		ret = wait(&status);
+		#else
+		ret = waitpid(pid, &status, 0);
+		#endif
+		printf("wait end, ret=%d, status=%d\n", ret, status);
+		exit(0);
+	}
 }
