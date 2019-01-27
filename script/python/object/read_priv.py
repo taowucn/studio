13a68f04221c5e439c2426b1a2a7268352f157b4
Tao Wu <wtomybo@gmail.com>
2019-01-27 21:53:41 +0800

    add python/object sample

---
+++ script/python/object/read_priv.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python
+# -*- coding: UTF-8 -*-
+
+class Runoob:
+    __site = "www.runoob.com"
+
+runoob = Runoob()
+print runoob._Runoob__site
---
+++ script/python/object/t.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+
+class Test:
+    def prt(self):
+        print(self)
+        print(self.__class__)
+ 
+t = Test()
+t.prt()
+
