57752635e89e719e918099d7df4ae71ce444ac0f
Tao Wu <wtomybo@gmail.com>
2019-03-31 22:52:15 +0800

    python/numpy: update slice.py

--- script/python/numpy/slice.py
+++ script/python/numpy/slice.py
@@ -7,8 +7,9 @@ b = a[2:7:2]
 print (a)
 print (b)
 print (a[2:])
+print (a[:-1])
 
-a = np.array([[1,2,3],[3,4,5],[4,5,6]])
+a = np.array([[1,2,3],[4,5,6],[7,8,9]])
 print (a)
 print (a[1:])
 
@@ -16,3 +17,6 @@ print (a[...,1])   # 2th column
 print (a[1,...])   # 2th row
 print (a[...,1:])  # all elements after 2th column
 
+a = np.array([[1,2,3],[4,5,6],[7,8,9]])
+print ("Test:")
+print (a[:,1])
