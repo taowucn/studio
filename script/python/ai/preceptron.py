22fc381f07e515866aeaf63ec284e27f73a9cdde
Tao Wu <wtomybo@gmail.com>
2019-05-26 19:48:56 +0800

    python/ai: Add verbose info in preceptron

--- script/python/ai/preceptron.py
+++ script/python/ai/preceptron.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python
+#!/usr/bin/env python
 #coding=utf-8
 
 class Perceptron(object):
@@ -12,11 +12,13 @@ class Perceptron(object):
         self.weights = [0.0 for _ in range(input_num)]
         # 偏置项初始化为0
         self.bias = 0.0
+
     def __str__(self):
         '''
         打印学习到的权重、偏置项
         '''
         return 'weights\t:%s\nbias\t:%f\n' % (self.weights, self.bias)
+
     def predict(self, input_vec):
         '''
         输入向量，输出感知器的计算结果
@@ -30,12 +32,14 @@ class Perceptron(object):
                    map(lambda (x, w): x * w,  
                        zip(input_vec, self.weights))
                 , 0.0) + self.bias)
+
     def train(self, input_vecs, labels, iteration, rate):
         '''
         输入训练数据：一组向量、与每个向量对应的label；以及训练轮数、学习率
         '''
         for i in range(iteration):
             self._one_iteration(input_vecs, labels, rate)
+
     def _one_iteration(self, input_vecs, labels, rate):
         '''
         一次迭代，把所有的训练数据过一遍
@@ -43,12 +47,14 @@ class Perceptron(object):
         # 把输入和输出打包在一起，成为样本的列表[(input_vec, label), ...]
         # 而每个训练样本是(input_vec, label)
         samples = zip(input_vecs, labels)
+
         # 对每个样本，按照感知器规则更新权重
         for (input_vec, label) in samples:
             # 计算感知器在当前权重下的输出
             output = self.predict(input_vec)
             # 更新权重
             self._update_weights(input_vec, output, label, rate)
+
     def _update_weights(self, input_vec, output, label, rate):
         '''
         按照感知器规则更新权重
@@ -62,12 +68,14 @@ class Perceptron(object):
             zip(input_vec, self.weights))
         # 更新bias
         self.bias += rate * delta
+        #print('[Train] weights: {}, bias: {}, rate: {}, delta: {}'.format(self.weights, self.bias, rate, delta))
 
 def f(x):
     '''
     定义激活函数f
     '''
     return 1 if x > 0 else 0
+
 def get_training_dataset():
     '''
     基于and真值表构建训练数据
@@ -79,6 +87,7 @@ def get_training_dataset():
     # [1,1] -> 1, [0,0] -> 0, [1,0] -> 0, [0,1] -> 0
     labels = [1, 0, 0, 0]
     return input_vecs, labels    
+
 def train_and_perceptron():
     '''
     使用and真值表训练感知器
@@ -90,6 +99,7 @@ def train_and_perceptron():
     p.train(input_vecs, labels, 10, 0.1)
     #返回训练好的感知器
     return p
+
 if __name__ == '__main__': 
     # 训练and感知器
     and_perception = train_and_perceptron()
@@ -100,3 +110,4 @@ if __name__ == '__main__':
     print '0 and 0 = %d' % and_perception.predict([0, 0])
     print '1 and 0 = %d' % and_perception.predict([1, 0])
     print '0 and 1 = %d' % and_perception.predict([0, 1])
+
